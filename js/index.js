ed653bfa9eee4faf53da2f7cd595e2569d8d5cdc
Luis Pinheiro <profitcreations@live.com>
2017-05-25 02:48:27 +0100

    Optimised Layout

--- js/index.js
+++ js/index.js
@@ -1,43 +1,50 @@
-
-$(document).ready(function(){
-  
-  var api = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search=";
+$(document).ready(function() {
+  var api =
+    "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search=";
   var searchTerm = $("#search");
   var term;
-  
-  $("#search").keypress(function(e){
-    // console.log(e);
-    if ( e.originalEvent.key === "Enter" ) {
-       term = api + searchTerm[0].value;
-       getWiki(term);
+
+  $("#search").keypress(function(e) {
+    if (e.originalEvent.key === "Enter") {
+      term = api + searchTerm[0].value;
+      getWiki(term);
     }
   });
-  
-  
-  $("#submit-button").click( function(){
+
+  $("#submit-button").click(function() {
     term = api + searchTerm[0].value;
-   getWiki(term);
+    getWiki(term);
   });
-  function getWiki( val ){
+  function getWiki(val) {
     $.ajax({
-    url: val,
-    jsonp: "callback",
-    dataType: "jsonp",
-    success: function(data) {
-      // Empty before inserting everything
-      // console.log(data);
-     for (var i = 0; i < data[1].length; i++) {
-       $("#results").prepend('<a href="' + data[3][i] + '" target="_blank"><div class="col-xs-12 center-block"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">' + data[1][i] + '</h3></div><div class="panel-body">' + data[2][i] + '</div></div></div></a>');
-     }
-    }
-  });
+      method: "GET",
+      url: val,
+      // jsonp: "callback",
+      dataType: "jsonp",
+      success: function(data) {
+        for (var i = 0; i < data[1].length; i++) {
+          $("#results").prepend(
+
+              '<div class="col-sm-6">'
+            +  '<div class="card">'
+            +    '<div class="card-block">'
+            +      '<h3 class="card-title">' + data[1][i] + '</h3>'
+            +      '<p class="card-text">' + data[2][i] + '</p>'
+            +      '<a href="' + data[3][i] + '" class="btn btn-primary" target="_blank">Read More</a>'
+            +    '</div>'
+            +  '</div>'
+            + '</div>'
+
+            /* '<a href="' +
+              data[3][i] +
+              '" target="_blank"><div class="col-xs-12 center-block"><div class="panel"><div class="panel-heading"><h3 class="panel-title">' +
+              data[1][i] +
+              '</h3></div><div class="panel-body">' +
+              data[2][i] +
+              "</div></div></div></a>" */
+          );
+        }
+      }
+    });
   }
-  
-  /* Random Search */
-  $("#rand-search").click(function() {
-    
-    
-    
-  });
-  
 });
